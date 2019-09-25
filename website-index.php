e0a71290973338a40bfff3da16993f8d181a9359
Laurendelvacchio211 <54918432+Laurendelvacchio211@users.noreply.github.com>
2019-09-25 17:41:33 -0400

    Add files via upload

---
+++ website-index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  	<meta charset="utf-8"/>
+	<title>History Website</title>
+	
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script|UnifrakturMaguntia&display=swap" rel="stylesheet">
+  	<link rel='stylesheet' href='css/styles.css' media='screen'>
+</head>
+   <body>
+     
+  <header>
+     
+  <nav>
+    <ul>
+    <li><a href ="#home">Home</a></li>
+    <li class="dropdown">
+		<a href='' class='dropbtn'>History</a>
+		<div class='dropdown-content'>
+			<a href="">Tudor Period</a>
+			<a href=''>Georgian Era</a>
+			<a href=''>Victorian Era</a>
+			<a href=''>World War 1</a>
+			<a href=''>World War 2</a>
+		</div>
+    <li><a href ="#places">Places</a></li>
+    <li><a href ="#About">About Me</a></li>
+    </ul>
+  </nav>
+  
+  <h1><b>Historical Britian</b></h1>
+  
+  </header>
+ 
+ 
+     <section>
+       <h2 class= 'spotlight'>Spotlight</h2>
+       <article class='spotlight-photos'>
+		   <a target='_black' href="tower-bridge.jpg">
+			   <img src= 'photos/tower-bridge.jpg' alt='Tower Bridge' width='400' height='200'></a>
+			   <div class ='desc'> Tower Bridge</div>
+	   </article>
+		 
+		 <article  class='spotlight-photos'>
+			 <a target='_blank' href='photos/york.jpg'>
+				 <img src='photos/york.jpg' alt='York' width='400' height='200'></a>
+				 <div class="desc">This is york</div>
+		</article>
+		 
+     </section>
+     
+     <section>
+		 <div class ='img-one'>
+       <img src='photos/henry-viii.jpg' alt='Henry the 8th'>
+   </div>
+   <div class ='img-one'>
+	   <img src ='photos/hampton.jpg' alt='Hampton Court Palace'>
+   </div>	
+     </section>
+     
+     <section>
+       <h2>Most Recent</h2>
+       <article>
+         <h4>Article 1</h4>
+       </article>
+       <article>
+         <h4>Article 2</h4>
+       </article>
+       <article>
+         <h4>Article 3</h4>
+       </article>
+     </section>
+     
+     
+    
+     
+     <footer>
+    <nav>
+      <h5>Navigation</h5>
+      <ul>
+        <li><a class ="active" href ="#home">Home</a></li>
+        <li><a class ="active" href="#History">History</a></li>
+        <li><a class ="active" href ="#places">Places</a></li>
+        <li><a class ="active" href ="#About">About Me</a></li>
+        <li><a class ="active" href ="#contact">Contact Us</a></li>
+      </ul>
+  </nav>
+     </footer>
+</body>
+</html>
