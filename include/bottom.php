45b81d841c74f5af20c3888b3476d18c169304f6
Laurendelvacchio211 <54918432+Laurendelvacchio211@users.noreply.github.com>
2019-11-19 10:28:32 -0500

    Add files via upload

---
+++ include/bottom.php
@@ -0,0 +1,22 @@
+<footer>
+    <nav>
+    <ul>
+    <li><a href ="../index.php">Home</a></li>
+    <li class="dropdown">
+		<a href='' class='dropbtn'>History</a>
+		<div class='dropdown-content'>
+			<a href="">Tudor Period</a>
+			<a href=''>Georgian Era</a>
+			<a href=''>Victorian Era</a>
+			<a href=''>World War 1</a>
+			<a href=''>World War 2</a>
+		</div>
+    <li><a href ="option2.php">Weather</a></li>            
+    <li><a href ="#places">Places</a></li>
+    <li><a href ="#About">About Me</a></li>
+	<li><a href ="contact/contact.php">Contact Us</a></li>
+    </ul>
+  </nav>
+     </footer>
+</body>
+</html>
---
+++ include/top.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152520706-1"></script>
+
+  	<meta charset="utf-8"/>
+	<title>History Website</title>
+
+	<link rel="stylesheet" href="../css/reset.css" />
+  	<link rel='stylesheet' href='../css/styles.css' media='screen'>
+	<link href="https://fonts.googleapis.com/css?family=Kaushan+Script|UnifrakturMaguntia&display=swap" rel="stylesheet">
+	
+        
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-151466105-1"></script>
+        
+
+        <meta http-equiv="Content-type" content="text/html; charset=utf-8"/>
+        <meta name="author" content="Shino Ito"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+   <body>
+    
+  <header>
+     
+  <nav>
+    <ul>
+    <li><a href ="../index.php">Home</a></li>
+    <li class="dropdown">
+		<a href='' class='dropbtn'>History</a>
+		<div class='dropdown-content'>
+			<a href="">Tudor Period</a>
+			<a href=''>Georgian Era</a>
+			<a href=''>Victorian Era</a>
+			<a href=''>World War 1</a>
+			<a href=''>World War 2</a>
+		</div>
+    <li><a href ="option2.php">Weather</a></li>            
+    <li><a href ="#places">Places</a></li>
+    <li><a href ="#About">About Me</a></li>
+	<li><a href ="contact/contact.php">Contact Us</a></li>
+    </ul>
+  </nav>
+  </header>
+  </body>
+ 
